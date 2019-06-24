e43c34f96e649e6810222d87aac78435519493f2
Aniruddha Maru <aniruddhamaru@gmail.com>
2019-06-24 15:59:57 -0700

    Remove alerting since it's now a separate resource

--- variables.tf
+++ variables.tf
@@ -6,30 +6,3 @@ variable "labels" {
   description = "Labels to attach to the PubSub topic and subscription"
   type        = "map"
 }
-
-variable "alerting" {
-  description = "Should Stackdriver alerts be generated?"
-  default     = "false"
-}
-
-variable "alerting_project" {
-  description = "The project where alerting resources should be created (defaults to current project)"
-  default     = ""
-}
-
-variable "queue_alarm_high_message_count_threshold" {
-  description = "Threshold for alerting on high message count in main queue"
-  default     = 5000
-}
-
-variable "queue_high_message_count_notification_channels" {
-  description = "Stackdriver Notification Channels for main queue alarm for high message count (required if alerting is on)"
-  type        = "list"
-  default     = []
-}
-
-variable "dlq_high_message_count_notification_channels" {
-  description = "Stackdriver Notification Channels for DLQ alarm for high message count (required if alerting is on)"
-  type        = "list"
-  default     = []
-}
