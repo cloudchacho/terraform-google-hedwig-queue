836a5bb7e6e8b83ef326923f511bc0e18c88d6f0
Aniruddha Maru <maru@standard.ai>
2020-01-29 12:05:22 -0800

    fix typo

--- main.tf
+++ main.tf
@@ -48,11 +48,11 @@ data "google_iam_policy" "subscription_policy" {
   }
 }
 
-resource "google_pubsub_topic_iam_policy" "subscription_policy" {
+resource "google_pubsub_subscription_iam_policy" "subscription_policy" {
   count = "${var.iam_service_account == "" ? 0 : 1}"
 
-  policy_data = "${data.google_iam_policy.subscription_policy.policy_data}"
-  topic       = "${google_pubsub_subscription.subscription.name}"
+  policy_data  = "${data.google_iam_policy.subscription_policy.policy_data}"
+  subscription = "${google_pubsub_subscription.subscription.name}"
 }
 
 resource "google_pubsub_topic" "dlq_topic" {
@@ -105,9 +105,9 @@ data "google_iam_policy" "dlq_subscription_policy" {
   }
 }
 
-resource "google_pubsub_topic_iam_policy" "dlq_subscription_policy" {
+resource "google_pubsub_subscription_iam_policy" "dlq_subscription_policy" {
   count = "${var.iam_service_account == "" ? 0 : 1}"
 
-  policy_data = "${data.google_iam_policy.dlq_subscription_policy.policy_data}"
-  topic       = "${google_pubsub_subscription.dlq_subscription.name}"
+  policy_data  = "${data.google_iam_policy.dlq_subscription_policy.policy_data}"
+  subscription = "${google_pubsub_subscription.dlq_subscription.name}"
 }
