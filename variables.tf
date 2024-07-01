108208f0857ce9d954d46df0998d4402edd40e2b
Aniruddha Maru <maru@standard.ai>
2024-07-01 14:35:02 -0700

    Add retry policy for default queue (#1)

--- variables.tf
+++ variables.tf
@@ -11,3 +11,12 @@ variable "iam_service_account" {
   description = "The IAM service account to create exclusive IAM permissions for the topic"
   default     = ""
 }
+
+variable "retry_policy" {
+  description = "A policy that specifies how Pub/Sub retries message delivery for this subscription. If not set, the default retry policy is applied. This generally implies that messages will be retried as soon as possible for healthy subscribers. RetryPolicy will be triggered on NACKs or acknowledgement deadline exceeded events for a given message"
+  type = object({
+    minimum_backoff = string
+    maximum_backoff = string
+  })
+  default = null
+}
